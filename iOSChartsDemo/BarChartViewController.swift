5793ee5a1c6d64c264cfcd8a609d2c618b96fc42
anhnv2011 <anhnguyenviet@anhs-MacBook-Pro-2.local>
2016-10-19 22:45:49 +0700

    update

--- iOSChartsDemo/BarChartViewController.swift
+++ iOSChartsDemo/BarChartViewController.swift
@@ -15,52 +15,8 @@ class BarChartViewController: UIViewController {
     
     @IBOutlet var barChartView: BarChartView!
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Get and prepare the data
-//        let sales = DataGenerator.data()
-//        
-//        // Initialize an array to store chart data entries (values; y axis)
-//        var salesEntries = [ChartDataEntry]()
-//        
-//        // Initialize an array to store months (labels; x axis)
-//        var salesMonths = [String]()
-//        
-//        var i = 0
-//        for sale in sales {
-//            // Create single chart data entry and append it to the array
-//            let saleEntry = BarChartDataEntry(x: Double(i), y: sale.value, data: salesMonths as AnyObject?)
-//            
-////            let saleEntry = BarChartDataEntry(x: Double(i), y: sale.value)
-//            salesEntries.append(saleEntry)
-//            
-//            // Append the month to the array
-//            salesMonths.append(sale.month)
-//            
-//            i += 1
-//            
-//        }
-//        
-//        
-//        // Create bar chart data set containing salesEntries
-//        let chartDataSet = BarChartDataSet(values: salesEntries, label: "kjhfjgcjgc")
-//        
-//        // Create bar chart data with data set and array with values for x axis
-//        let chartData = BarChartData(dataSets: [chartDataSet])
-//        
-////            BarChartData(xVals: salesMonths, dataSets: [chartDataSet])
-//        
-//        // Set bar chart data to previously created data
-//        barChartView.data = chartData
-//        
-//        
-//        
-//        barChartView.descriptionText = ""
-//        
-//    }
-//}
 
-//    
+   var avgUnit = 0.0
     public class BarChartFormatter: NSObject, IAxisValueFormatter{
         
         var months: [String]! = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -78,8 +34,18 @@ class BarChartViewController: UIViewController {
         super.viewDidLoad()
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
 //        BarChartFormatter()
+        var totalunit = 0.0
+        for i in unitsSold
+        {
+            totalunit += i
+        }
+        print(totalunit)
+        avgUnit = totalunit/(Double(months.count))
+        print(avgUnit)
+        
         setChart(dataPoints: months, values: unitsSold)
         
+       
     }
    
    
@@ -97,7 +63,7 @@ class BarChartViewController: UIViewController {
         }
         xaxis.valueFormatter = formato
 
-        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "number food")
         
         let chartData = BarChartData()
         chartData.addDataSet(chartDataSet)
@@ -107,11 +73,11 @@ class BarChartViewController: UIViewController {
         barChartView.xAxis.labelPosition = .bottom
         barChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
         barChartView.drawBarShadowEnabled = true
-        barChartView.animate(xAxisDuration: 1)
+        barChartView.animate(xAxisDuration: 2)
         
         
     
-        let ll = ChartLimitLine(limit: 12, label: "Target")
+        let ll = ChartLimitLine(limit: avgUnit , label: "Avg")
         barChartView.rightAxis.addLimitLine(ll)
     }
     
